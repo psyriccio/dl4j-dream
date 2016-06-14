9a93b7247c90914070b33d80d6c971a4f6099e77
John Gleezowood <psyriccio@gmail.com>
2016-06-14 22:29:39 +0300

    initial test skeleton

--- src/dl4jdream/Main.java
+++ src/dl4jdream/Main.java
@@ -3,7 +3,12 @@ package dl4jdream;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("!!!");
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        new MainFrame().setVisible(true);
+      }
+    });
   }
 
 }
