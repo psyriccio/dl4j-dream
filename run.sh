5bf5d04c0357132c73e358facf63901342240570
John Gleezowood <psyriccio@gmail.com>
2016-06-14 22:47:44 +0300

    upgrade run shell script: exec build.sh when compiled jar not exists

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ ! -f ./build/libs/dl4j-dream-latest-SNAPSHOT-all.jar ]
+
+  then
+    
+    ./build.sh
+
+fi
+
 java -jar ./build/libs/dl4j-dream-latest-SNAPSHOT-all.jar
+
